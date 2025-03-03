674e86cff8815e30eab79dd43d57870c4c78e11b
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-03-03 12:28:10 +0200

    Handeled the format of body respone

--- wallet-server/connection/connection.php
+++ wallet-server/connection/connection.php
@@ -15,3 +15,16 @@ $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+
+$data = [];
+// check if request is JSON or formdata before pass body to any api
+if ($_SERVER['REQUEST_METHOD'] === "GET" || $_SERVER['REQUEST_METHOD'] === 'DELETE') {
+  $data = $_GET;
+} else {
+  if (isset($_SERVER["CONTENT_TYPE"]) && $_SERVER["CONTENT_TYPE"] !== "application/json") {
+
+    $data = $_POST;
+  } else {
+    $data = json_decode(file_get_contents("php://input"), true) ?? [];
+  }
+}
