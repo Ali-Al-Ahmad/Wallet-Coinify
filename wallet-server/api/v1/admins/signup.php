a08b05478d13eb162cc39f38b9c9faa12a003b99
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-03-05 19:54:42 +0200

    Added signup Api

---
+++ wallet-server/api/v1/admins/signup.php
@@ -0,0 +1,14 @@
+<?php
+require_once "../../../connection/connection.php";
+require_once "../../../utils/utils.php";
+require_once "../../../models/adminModel.php";
+
+
+if (!isset($data["email"]) || !isset($data["password"])) {
+    die(responseError("Missing email or password!"));
+}
+
+$admin = new Admin();
+$response = $admin->signUp($data["email"], $data["password"]);
+
+echo $response;
