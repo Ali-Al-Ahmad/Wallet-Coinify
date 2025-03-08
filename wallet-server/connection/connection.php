fe9916ff8478564f9993916983272730edf1a56f
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-03-08 16:22:37 +0200

    allow all headers

--- wallet-server/connection/connection.php
+++ wallet-server/connection/connection.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Content-Type: application/json");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: *");
 
 
 $db_host = "localhost";
